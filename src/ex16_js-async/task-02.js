94a12839c45295545cf2d2f83ae3598031909374
Maria Malistova <malistovam@mail.ru>
2019-11-21 20:48:54 +0300

    ex16 js async complete

--- src/ex16_js-async/task-02.js
+++ src/ex16_js-async/task-02.js
@@ -8,13 +8,12 @@ function debounce(func, delay) {
             func.apply(context, args);
         }), delay);
     } 
-}  
+}
 let input = document.querySelector('input');
 input.addEventListener('input', debounce(find, 2000)); 
 
 function find() {  
-    let row = document.getElementsByTagName("td");
-     
+    let row = document.getElementsByTagName("td");    
     for (let i = 0; i < row.length; i++) {
         if (row[i].innerHTML.toLowerCase().indexOf(input.value.toLowerCase()) === -1) {
             row[i].parentElement.style.display = "none";
